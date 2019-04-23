17180a6721019de859bb77e678c6f949c545d821
Yiwen Wang <vicey@live.com>
2019-04-23 12:28:23 +0800

    Update

--- bond-parser/src/Syntax/Syntax.ts
+++ bond-parser/src/Syntax/Syntax.ts
@@ -1,5 +1,5 @@
-import { Token } from "src/Lexical";
-import { TextSpan } from "src/Document";
+import { Token } from "../Lexical";
+import { TextSpan } from "../Document";
 
 export enum SyntaxType {
     // Non-top level syntaxes
@@ -11,13 +11,23 @@ export default abstract class Syntax extends TextSpan {
     public type: SyntaxType;
 
     protected tokens: Token[];
+    protected syntaxes?: Syntax[];
 
-    public constructor(type: SyntaxType, tokens: Token[]) {
+    public constructor(type: SyntaxType, tokens?: Token[], syntaxes?: Syntax[]) {
+        if (tokens !== undefined) {
+            // No op
+        } else if (syntaxes !== undefined) {
+            tokens = syntaxes.flatMap(syntax => syntax.tokens);
+        } else {
+            throw new Error("One of tokens or syntaxes must be provided.");
+        }
+
         const spanStart = tokens[0].start;
         const spanEnd = tokens[tokens.length - 1].end;
         const spanLength = spanEnd - spanStart;
         super(spanStart, spanLength);
         this.type = type;
         this.tokens = tokens;
+        this.syntaxes = syntaxes;
     }
 }
